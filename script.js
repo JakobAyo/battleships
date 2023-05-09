af3ecc8ac7adcbb6dced6c48f8e06c5c57e2b6c3
YacoubAyo <yacoubayo@gmx.de>
2023-05-09 11:29:05 +0200

    draggable ships

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const gridCells = document.querySelectorAll('.cpu-player .grid-cell');
+const cpuGridCells = document.querySelectorAll('.cpu-player .grid-cell');
+const mainGridCells = document.querySelectorAll('.main-player .grid-cell');
+const ships = document.querySelectorAll('.ship');
 
-gridCells.forEach(gridCell => {
+
+cpuGridCells.forEach(gridCell => {
   gridCell.addEventListener('click', (event) => {
     event.target.style.visibility = 'hidden';
-  });
-});
+  })
+})
+
+ships.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
+})
+
+mainGridCells.forEach(cell => {
+  cell.addEventListener('dragover', e => {
+    e.preventDefault
+    const draggable = document.querySelector('.dragging')
+    cell.appendChild(draggable)
+  })
+})
