2af50ce0817290c58ceddcda98df318722fff805
YacoubAyo <yacoubayo@gmx.de>
2023-05-21 11:42:51 +0200

    update cell status

--- script.js
+++ script.js
@@ -1,60 +1,123 @@
-const cpuGridCells = document.querySelectorAll('.cpu-player .grid-cell')
-const mainGridCells = document.querySelectorAll('.main-player .grid-cell')
-const ships = document.querySelectorAll('.ship')
+var CONST = {}
 
-const rotateButton = document.querySelector('#rotate-button')
+CONST.AVAILABLE_SHIPS = ['destroyer', 'submarine', 'cruiser', 'battleship', 'carrier']
 
-const destroyer = 2
-const submarine = 3
-const cruiser = 3
-const battleship = 4
-const carrier = 5
+CONST.HUMAN_PLAYER = 0
+CONST.COPMUTER_PLAYER = 1
+
+CONST.CSS_EMPTY = 'empty'
+CONST.CSS_SHIP = 'ship'
+CONST.CSS_MISS = 'miss'
+CONST.CSS_HIT = 'hit'
+CONST.CSS_SUNK = 'sunk'
+
+CONST.EMTPY = 0
+CONST.SHIP = 1
+CONST.MISS = 2
+CONST.HIT = 3
+CONST.SUNK = 4
 
 
-// clicking hides the cell
-cpuGridCells.forEach(gridCell => {
-  gridCell.addEventListener('click', (event) => {
-    event.target.style.visibility = 'hidden'
-  })
-})
-
-
-ships.forEach(ship => {
-  ship.addEventListener('dragstart', handleDragStart)
-  ship.addEventListener('dragend', handleDragEnd)
-})
-
-mainGridCells.forEach(cell => {
-  cell.addEventListener('dragenter', handleDragEnter)
-  cell.addEventListener('dragover', handleDragOver)
-  cell.addEventListener('dragleave', handleDragLeave)
-  cell.addEventListener('drop', handleDrop)
-})
-
-function handleDragStart(event) {
-  event.currentTarget.classList.add('dragging')
+function Game() {
+    this.humanGrid = new Grid()
+    this.computerGrid = new Grid()
+    this.humanFleet = new Fleet(this.humanGrid, CONST.HUMAN_PLAYER)
+    this.computerFleet = new Fleet(this.computerFleet, CONST.COPMUTER_PLAYER)
 }
 
-function handleDragEnd(event) {
-  event.currentTarget.classList.remove('dragging')
+
+function Grid() {
+    this.cells = []
+    this.init()
 }
 
-function handleDragEnter(event) {
-  event.preventDefault()
+
+Grid.prototype.init = function() {
+    for (let x = 0; x < 10; x++) {
+        let row = []
+        this.cells[x] = row
+        for (let y = 0; y < 10; y++) {
+            row.push(CONST.EMTPY)
+        }
+    }
+
 }
 
-function handleDragOver(event) {
-  event.preventDefault()
+Grid.prototype.updateCell = function(x, y, shipType) {
+    let player
+    
+    switch (shipType) {
+        case CONST.CSS_EMPTY:
+            this.cells[x][y] = CONST.EMTPY
+            break
+        case CONST.CSS_SHIP:
+            this.cells[x][y] = CONST.SHIP
+            break
+        case CONST.CSS_MISS:
+            this.cells[x][y] = CONST.MISS
+            break
+        case CONST.CSS_HIT:
+            this.cells[x][y] = CONST.HIT
+            break
+        case CONST.CSS_SUNK:
+            this.cells[x][y] = CONST.SUNK
+            break
+        default:
+            this.cells[x][y] = CONST.EMTPY
+            break
+    }
 }
 
-function handleDragLeave(event) {
-  const index = dragEnteredCells.indexOf(event.currentTarget)
-  if(index > -1) {
-    dragEnteredCells.splice(index, 1)
-  }
+Grid.prototype.setCSS = function(player) {
+    let playerCSS
+    if (player === CONST.HUMAN_PLAYER){
+        playerCSS = '.main-player'
+    } else {
+        playerCSS = '.cpu-player'
+    }
+    for (let x = 0; x < 10; x++){
+        for (let y = 0; y < 10; y++) {
+            let cell = this.cells[x][y]
+            
+            let cssCell = document.querySelector(`${playerCSS} .grid-cell${x}-${y}`)
+            
+            switch (cell) {
+                case CONST.EMTPY:
+                    cssCell.classList.add(CONST.CSS_EMPTY)
+                    break
+                case CONST.SHIP:
+                    cssCell.classList.add(CONST.CSS_SHIP)
+                    break
+                case CONST.MISS:
+                    cssCell.classList.add(CONST.CSS_MISS)
+                    break
+                case CONST.HIT:
+                    cssCell.classList.add(CONST.CSS_HIT)
+                    break
+                case CONST.SUNK:
+                    cssCell.classList.add(CONST.SUNK)
+                    break
+            }
+        }
+    }
 }
 
-function handleDrop(event) {
-  event.preventDefault()
-  
+function Fleet(playerGrid, player) {
+    this.numShips = CONST.AVAILABLE_SHIPS
+    this.playerGrid = playerGrid
+    this.player = player
 }
+
+var game = new Game()
+
+game.humanGrid.updateCell(3, 1, 'miss')
+game.humanGrid.updateCell(0, 2, 'miss')
+game.humanGrid.updateCell(6, 3, 'miss')
+game.humanGrid.updateCell(0, 4, 'miss')
+game.humanGrid.updateCell(0, 5, 'miss')
+game.humanGrid.updateCell(1, 6, 'miss')
+game.humanGrid.updateCell(0, 7, 'miss')
+game.humanGrid.setCSS()
+
+game.computerGrid.updateCell(0, 0, 'miss')
+game.computerGrid.setCSS()
